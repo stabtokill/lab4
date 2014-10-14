cc9305d77f4c6e48814350c08914fcaf02e060f4
daniel.s.hoffman <daniel.s.hoffman@CL-QBB00116-25.ad.ndsu.edu>
2014-10-14 11:47:34 -0500

    added stuff to project class.

--- src/lab4/Project.java
+++ src/lab4/Project.java
@@ -6,5 +6,33 @@ package lab4;
  * @author 
  */
 public class Project {
+    private int proj_idNumber; 
+    private String proj_name;
+    private int proj_manager;
+    private String proj_startDate;
+    private String proj_endDate;
+    // variables
+    int[] proj_assignments = new int[10];
+    // our array for our assisgnments
+    
+    private int getproj_idNumber(){
+        return proj_idNumber;
+    }
+    private String getproj_name(){
+        return proj_name;
+    }
+    private int getproj_manager(){
+        return proj_manager;
+    }
+    public String getproj_startDate(){
+        return proj_startDate;
+    }
+    public String getproj_endDate(){
+        return proj_endDate;
+    }
+    
+    
+    
+    
     
 }
