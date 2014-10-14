5bd49cef78617e4f01b34b9c8d117f35d1baa141
daniel.zimmer <daniel.zimmer@CL-QBB00116-24.ad.ndsu.edu>
2014-10-14 11:42:29 -0500

    employee variables and methods

--- src/lab4/Employee.java
+++ src/lab4/Employee.java
@@ -11,4 +11,66 @@ public class Employee
  private int idnumber;
  private String name;
  private String gender;
+ private int age;
+ private int salary;
+ private String jobTitle;
+
+ public void setidnumber(int id)
+ {
+     idnumber = id;
+ }
+ public void setname(String n)
+ {
+     name = n;
+ }
+ public void setgender(String g)
+ {
+     gender = g;
+ }
+ public void setage(int a)
+ {
+     age = a;
+ }
+ public void setsalary(int s)
+ {
+     salary = s;
+ }
+ public void setjobTitle(String jt)
+ {
+     jobTitle = jt; 
+ }
+ public int getidnumber()
+ {
+     return idnumber;
+ }
+ public String getname()
+ {
+     return name;
+ }
+ public String getgender()
+ {
+     return gender;
+ }
+ public int getage()
+ {
+     return age;
+ }
+ public int getsalary()
+ {
+     return salary;
+ }
+ public String getjobTitle()
+ {
+     return jobTitle;
+ }
+ private void promote(String jobtitle)
+ {
+     
+ }
+ private void increment(int salary)
+ {
+     
+ }
+         
+ 
 }
