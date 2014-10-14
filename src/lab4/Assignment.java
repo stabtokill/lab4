071d65e4225df66e06b2ee79a449a4132372862e
brian.silewski <brian.silewski@CL-QBB00116-26.ad.ndsu.edu>
2014-10-14 11:29:40 -0500

    added variables and get and set methods for variables

--- src/lab4/Assignment.java
+++ src/lab4/Assignment.java
@@ -8,4 +8,43 @@ package lab4;
  */
 public class Assignment {
     
+    private int idNumber;
+    private String startDate;
+    private String endDate;
+    private int[] employeeSize = new int[5];//TODO change this
+    private int salary;
+    
+    
+    
+    
+    public int getidNumber(){
+        return idNumber;
+    }
+    
+    public String getStartDate(){
+        return startDate;
+    }
+    
+    public String getEndDate(){
+        return endDate;
+    }
+    
+    public int getEmployee(int x){
+        x++;
+        return employeeSize[x];
+    }
+    
+    public void getConsultant(){ //TODO fix this
+    
+    }
+   
+    
+    public int getSalary(){
+        return salary;
+    }
+    
+    
+    
+    
+    
 }
