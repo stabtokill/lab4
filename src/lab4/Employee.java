58f8581ffe6f055592cc8e40e9ad52f64729339f
daniel.s.hoffman <daniel.s.hoffman@CL-QBB00116-25.ad.ndsu.edu>
2014-10-16 11:14:42 -0500

    I documented most of this class.

--- src/lab4/Employee.java
+++ src/lab4/Employee.java
@@ -14,7 +14,8 @@ public class Employee
  private int age;
  private int salary;
  private String jobTitle;
-
+// variables that store our data
+ 
  public void setidnumber(int id)
  {
      idnumber = id;
@@ -39,6 +40,9 @@ public class Employee
  {
      jobTitle = jt; 
  }
+ 
+    // set functions recieve vairables and set them equal to a new variable 
+    // names. from there they are stored in those new variables.
  public int getidnumber()
  {
      return idnumber;
@@ -63,6 +67,10 @@ public class Employee
  {
      return jobTitle;
  }
+ 
+    // the get functions take the new variables and send them off to other 
+    // classes when they are called.
+ 
  private void promote(String jobtitle)
  {
      
