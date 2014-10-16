11f29543508fad766ab297453bac71a32d4b459e
daniel.zimmer <daniel.zimmer@CL-QBB00116-24.ad.ndsu.edu>
2014-10-16 11:38:58 -0500

    changes to Employee

--- src/lab4/Consultant.java
+++ src/lab4/Consultant.java
@@ -6,6 +6,17 @@ package lab4;
  *
  * @author 
  */
-public class Consultant {
+public class Consultant extends Employee
+{
+    private String startDate;
+    private String endDate;
     
+    public void setsartDate(String s)
+    {
+        startDate = s;
+    }
+    public void setendDate(String e)
+    {
+        endDate = e;
+    }
 }
