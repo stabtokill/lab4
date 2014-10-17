14ce6f6c012f39fec5a230b98067bdfeff7c21b6
daniel.s.hoffman <daniel.s.hoffman@CL-QBB00116-25.ad.ndsu.edu>
2014-10-16 20:46:47 -0500

    started info

--- src/lab4/Lab4.java
+++ src/lab4/Lab4.java
@@ -2,18 +2,41 @@
 
 package lab4;
 
+import java.util.Scanner;
+
 /**
  *  section 2
- * Zimmer, last name
+ * Zimmer, Hoffman, last name
  */
 public class Lab4 {
 
-    /**
-     * @param args the command line arguments
-     */
+   
     public static void main(String[] args) {
-        // TODO code application logic here55555
-        //stufffhggggggggggggggg
+       Scanner input = new Scanner(System.in);
+       String[] employee1;
+       employee1 = new String[5];
+       System.out.print("Please enter your ID");
+       String s = input.nextLine();
+       employee1[0] = s;
+       System.out.print("Please enter your Name");
+       String n = input.nextLine();
+       employee1[1] = n;
+       System.out.print("Please enter your age");
+       String a = input.nextLine();
+       employee1[2] = a;
+       System.out.print("Please enter your gender");
+       String g = input.nextLine();
+       employee1[3] = g;
+       System.out.print("Please enter your salary");
+       String m = input.nextLine();
+       employee1[4] = m;
+       System.out.print("Please enter your job title");
+       String jt = input.nextLine();
+       employee1[5] = jt;
+       System.out.print(employee1[]);
+       
+       
+       
     }
     
 }
