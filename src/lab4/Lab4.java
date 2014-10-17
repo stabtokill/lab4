dc6dedafff8917159cf0787a4b1ce259c050a70d
daniel.zimmer <daniel.zimmer@CL-QBB00114-08.ad.ndsu.edu>
2014-10-17 01:20:53 -0500

    suchsess

--- src/lab4/Lab4.java
+++ src/lab4/Lab4.java
@@ -1,84 +1,86 @@
-
-
 package lab4;
 
 import java.util.Scanner;
 
 /**
- *  section 2
- * Zimmer, Hoffman, last name
+ * section 2 
+ * Zimmer, Hoffman, Silewski
  */
 public class Lab4 {
 
-   public static Scanner s = new Scanner(System.in);
-   private static boolean newEmployee = false;
-   private static boolean newManager = false;
-   private static boolean newConsultant = false;
-    
-   public static void main(String[] args) 
-    {
+    public static Scanner s = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        //making the right sized array for employee.
         System.out.println("how many employees will you be entering have?");
         int emp = s.nextInt();
         Employee e[] = new Employee[emp];
+        //filling the array of employees.
+        for(int i =0; i<emp; i++){
+          e[i] = new Employee();
+          enterInEmployee(e[i]);         
+        }
+        //making the right sized array for managers.
+        System.out.println("how many Managers will you be entering have?");
+        int manage = s.nextInt();
+        Manager m[] = new Manager[manage];
+       //filling the array of managers.
+        for(int i =0; i<=manage; i++){
+          e[i] = new Employee();
+          enterInEmployee(m[i]);
+          enterInManager(m[i]);
+        }
+        //making the right sized array for consultants.
+        System.out.println("how many Consultants will you be entering have?");
+        int consult = s.nextInt();
+        Consultant c[] = new Consultant[consult];
+       //filling the array of consulatants.
+        for(int i =0; i<=consult; i++){
+          c[i] = new Consultant();
+          enterInEmployee(c[i]);
+          enterInConsult(c[i]);
+        }
         
-        String temp;
-        int i = 1;
-       while (false == newEmployee)
-       {
-          System.out.println("Employee? Y/N");
-           temp = s.next();
-           if (temp.equals("Y"))
-           {
-           e[i] = new Employee();
-           enterInEmployee(e[i]);
-           i++;
-           }
-           else
-             newEmployee = true;
-       }
-       
-       Manager man1 = new Manager();
-       enterInEmployee(man1);
-       enterInManager(man1);
-    }  
-    public static void enterInEmployee(Employee x)
-    {   
-       System.out.print("Please enter your ID: ");
-       x.setidnumber(s.nextInt());
-       
-       System.out.print("Please enter your Name: ");
-       x.setname(s.next());
-       
-       System.out.print("Please enter your age: ");
-       x.setage(s.nextInt());
-       
-       System.out.print("Please enter your gender: ");
-       x.setgender(s.next());
-       
-       System.out.print("Please enter your salary: ");
-       x.setsalary(s.nextDouble());
-       
-       System.out.print("Please enter your job title: ");
-       x.setjobTitle(s.next());
-       
     }
-    public static void enterInManager(Manager x)
-    {      
+       //setting all the information for Employee and its extends
+    public static void enterInEmployee(Employee x) {
+        System.out.print("Please enter your ID: ");
+        x.setidnumber(s.nextInt());
+
+        System.out.print("Please enter your Name: ");
+        x.setname(s.next());
+
+        System.out.print("Please enter your age: ");
+        x.setage(s.nextInt());
+
+        System.out.print("Please enter your gender: ");
+        x.setgender(s.next());
+
+        System.out.print("Please enter your salary: ");
+        x.setsalary(s.nextDouble());
+
+        System.out.print("Please enter your job title: ");
+        x.setjobTitle(s.next());
         
-        Employee man1 = new Manager();
+        System.out.println("");
+    }
+    //setting manager infomation.
+    public static void enterInManager(Manager x) {
         System.out.print("Please enter your manager ID: ");
         x.setmanagerId(s.nextInt());
+        System.out.println("");
     }
-    public static void enterInConsult(Consultant x)
-    {
+    //setting consultant information.
+    public static void enterInConsult(Consultant x) {
         System.out.print("Please enter Consultant id: ");
-        x.setConsultantId(s.nextInt()); 
-        
+        x.setConsultantId(s.nextInt());
+
         System.out.print("Please enter statr date: ");
         x.setsartDate(s.next());
-        
+
         System.out.print("Please enter end date: ");
         x.setendDate(s.next());
+        System.out.println("");
     }
-    
+
 }
