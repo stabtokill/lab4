e841a88d8477775fb25f67117fc15f20f53a971a
daniel.zimmer <daniel.zimmer@CL-QBB00116-27.ad.ndsu.edu>
2014-10-17 00:06:53 -0500

    stuff

--- src/lab4/Manager.java
+++ src/lab4/Manager.java
@@ -8,8 +8,17 @@ package lab4;
  */
 public class Manager extends Employee
 {
-    private int projectId = 0;        //what project is this manager working on
+    private int projectId;  //what project is this manager working on
+    private int managerId;
     
+    public void setmanagerId(int mId)
+    {
+        managerId = mId;
+    }
+    public int getmanagerId()
+    {
+        return managerId;
+    }
     public void setprojectId(int pId)
     {
         if(projectId == 0)
