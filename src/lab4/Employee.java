cb041c200db043205fe11980a94116ecd27ee325
brian.silewski <brian.silewski@CL-QBB00116-26.ad.ndsu.edu>
2014-10-16 21:48:00 -0500

    more test cases

--- src/lab4/Employee.java
+++ src/lab4/Employee.java
@@ -1,106 +1,100 @@
-
-
 package lab4;
 
 /**
- * section 2
- * Zimmer, Hoffman, last name 
+ * section 2 Zimmer, Hoffman, Silewski
  */
-public class Employee 
-{
- private int idnumber;
- private String name;
- private String gender;
- private int age;
- private int salary;
- private String jobTitle;
+public class Employee {
+
+    private int idnumber;
+    private String name;
+    private String gender;
+    private int age;
+    private double salary;
+    private String jobTitle;
 // variables that store our data
- 
- public void setidnumber(int id)
- {
-     idnumber = id;
- }
- public void setname(String n)
- {
-     name = n;
- }
- public void setgender(String g)
- {
-     gender = g;
- }
- public void setage(int a)
- {
-     age = a;
- }
- public void setsalary(int s)
- {
-     salary = s;
- }
- public void setjobTitle(String jt)
- {
-     jobTitle = jt; 
- }
- /*
-    set functions recieve vairables and set them equal to a new variable 
-    names. from there they are stored in those new variables.
- */
- public int getidnumber()
- {
-     return idnumber;
- }
- public String getname()
- {
-     return name;
- }
- public String getgender()
- {
-     return gender;
- }
- public int getage()
- {
-     return age;
- }
- public int getsalary()
- {
-     return salary;
- }
- public String getjobTitle()
- {
-     return jobTitle;
- }
- /*
-    the get functions take the new variables and send them off to other 
-    classes when they are called.
- */
- private void promote(String jobtitle)
- {
-     
- }
- private void increment(int salary)
- {
-     
- }
- public void getEmployee()
- {
-     //what needs to be returned?
- }
- 
+
+    public void setidnumber(int id) {
+        idnumber = id;
+    }
+
+    public void setname(String n) {
+        name = n;
+    }
+
+    public void setgender(String g) {
+        gender = g;
+    }
+
+    public void setage(int a) {
+        age = a;
+    }
+
+    public void setsalary(double s) {
+        salary = s;
+    }
+
+    public void setjobTitle(String jt) {
+        jobTitle = jt;
+    }
+    /*
+     set functions recieve vairables and set them equal to a new variable 
+     names. from there they are stored in those new variables.
+     */
+
+    public int getidnumber() {
+        return idnumber;
+    }
+
+    public String getname() {
+        return name;
+    }
+
+    public String getgender() {
+        return gender;
+    }
+
+    public int getage() {
+        return age;
+    }
+
+    public double getsalary() {
+        return salary;
+    }
+
+    public String getjobTitle() {
+        return jobTitle;
+    }
+    /*
+     the get functions take the new variables and send them off to other 
+     classes when they are called.
+     */
+
+    public void getEmployee() {
+        //what needs to be returned?
+    }
+
     // the get functions take the new variables and send them off to other 
     // classes when they are called.
- 
- //promotes employees to managers.
- private void promote()
- {   
-         jobTitle = "manager";
-         
-         //set the newly apointed manager onto a project.
-         increment();
- }
- //increase the employee salary by twenty prcent.
- private void increment()
- {
-     salary = (int) (salary + salary* 0.2);
- }
+    private void promote() {
+        //promote gives more money
+        increment();
+    }
 
- 
+    //increase the employee salary by twenty prcent.
+
+    private void increment() {
+        salary = (int) (salary + salary * 0.2);
+    }
+
+    public String toString(){
+        String g;
+        g = "ID Number: " + idnumber;
+        g+= "\nName: " + name;
+        g+= "\nAge: " + age;
+        g+= "\nGender: " + gender;
+        g+= "\nSalary: $" + salary;
+        g+= "\nJob Title: " + jobTitle + "\n";
+       
+        return g;
+    }
 }
--- src/lab4/Lab4.java
+++ src/lab4/Lab4.java
@@ -10,34 +10,64 @@ import java.util.Scanner;
  */
 public class Lab4 {
 
+   public static Scanner s = new Scanner(System.in);
    
     public static void main(String[] args) {
-       Scanner input = new Scanner(System.in);
-       String[] employee1;
-       employee1 = new String[5];
+       Employee e1 = new Employee();
+       Employee e2 = new Employee();
+       Employee e3 = new Employee();
+       Employee e4 = new Employee();
+       Employee e5 = new Employee();
+       Employee e6 = new Employee();
+       Employee e7 = new Employee();
+       Employee e8 = new Employee();
+       Employee e9 = new Employee();
+       Employee e10 = new Employee();
+   
+       enterInEmployee(e1);
+       enterInEmployee(e2);
+       enterInEmployee(e3);
+       enterInEmployee(e4);
+       enterInEmployee(e5);
+       enterInEmployee(e6);
+       enterInEmployee(e7);
+       enterInEmployee(e8);
+       enterInEmployee(e9);
+       enterInEmployee(e10);
        
-       System.out.print("Please enter your ID");
-       employee1[0] = input.nextLine();
-       
-       System.out.print("Please enter your Name");
-       employee1[1] = input.nextLine();
-       
-       System.out.print("Please enter your age");
-       employee1[2] = input.nextLine();
-       
-       System.out.print("Please enter your gender");
-       employee1[3] = input.nextLine();
-       
-       System.out.print("Please enter your salary");
-       employee1[4] = input.nextLine();
-       
-       System.out.print("Please enter your job title");
-       employee1[5] = input.nextLine();
-       
-       System.out.print(employee1[]);
+       System.out.print(e1.toString());
+       System.out.print(e2.toString());
+       System.out.print(e3.toString());
+       System.out.print(e4.toString());
+       System.out.print(e5.toString());
+       System.out.print(e6.toString());
+       System.out.print(e7.toString());
+       System.out.print(e8.toString());
+       System.out.print(e9.toString());
+       System.out.print(e10.toString());       
        
        
+    }
+    
+    public static void enterInEmployee(Employee x){
+        
+       System.out.print("Please enter your ID: ");
+       x.setidnumber(s.nextInt());
        
+       System.out.print("Please enter your Name: ");
+       x.setname(s.next());
+       
+       System.out.print("Please enter your age: ");
+       x.setage(s.nextInt());
+       
+       System.out.print("Please enter your gender: ");
+       x.setgender(s.next());
+       
+       System.out.print("Please enter your salary: ");
+       x.setsalary(s.nextDouble());
+       
+       System.out.print("Please enter your job title: ");
+       x.setjobTitle(s.next());
     }
     
 }
